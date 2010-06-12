2b3efcf0dfc931502a94edc853c68c399b280911
Konstantin Käfer <github@kkaefer.com>
2010-06-12 23:31:01 +0100

    move tweets to apachesolr

---
+++ api/solr_objects.js
@@ -0,0 +1,30 @@
+var x = {
+  "tweet": {
+    "id" : "tweet-0641723445",
+    "type": "tweet",
+    "user_id": 39472334,
+    "links": ["link-Kyerisdlhjsyuwer"],
+    "text": "Here's a cool video: http://bit.ly/kSh3ks Check it out",
+    "client_name": "Twitterrific",
+    "client_url": "http://twitterific.com",
+    "created_at": "Sun Aug 30 14:46:33 +0000 2009",
+    "hashtag": ['test', 'imho'],
+    "mentions": ["Gernot"],
+    "in_reply_to_user_id": 710613,
+    "in_reply_to_screen_name": "Gernot",
+    "in_reply_to_status_id": 15099908654,
+    "favorited": false
+  },
+
+  "link": {
+    "id": "link-Kyerisdlhjsyuwer", // (md5-hash of the url)
+    "type": "url",
+    "url": ["http://www.youtube.com/watch?v=spiKgkW1UmI", "http://bit.ly/kSh3ks"],
+    "website": "youtube.com",
+    "created_at": "Fri Apr 8 14:46:33 +0000 2010",
+    "text": "We propose direct touch surfaces that are orders of magnitude larger than tabletops by integrating high-resolution multi-touch sensing into back-projected floors....",
+    "link_handler": "youtube",
+    "user_id": [ 39472334 ],
+    "playback_length": 934
+  }
+};
