5fd2ff4e6cf863d3ee0424842c57915f94667e72
Konstantin Käfer <github@kkaefer.com>
2010-06-13 13:29:25 +0100

    more changes

--- public/js/core.js
+++ public/js/core.js
@@ -23,14 +23,14 @@ $(function() {
   var max_pages = 16; // 3200 tweets = 16 pages
   var tweets_per_call = 200; // 200 is twitter limit on this
   var retryLimit = 3;
-  
+
   var max_tweets = (max_pages) * 200;
   var username = $("#username").val();
   var user_id;
-  var fetched_tweets = 0;  
-  
+  var fetched_tweets = 0;
+
   // fetch user
-  var url = twitterAPI + "users/show.json";  
+  var url = twitterAPI + "users/show.json";
   $.ajax({
     url: url,
     cache: false,
@@ -43,24 +43,25 @@ $(function() {
       $("#screen_name").text("(" + data.screen_name + ")");
       $("#total-tweets-number").text(data.statuses_count);
 
-      $("#fetched-tweets").text('0');      
+      $("#fetched-tweets").text('0');
       if(data.statuses_count > max_tweets) {
-        $("#total-available-tweets").text(max_tweets);            
+        $("#total-available-tweets").text(max_tweets);
       } else {
-        $("#total-available-tweets").text(data.statuses_count);                    
-      }     
-      
+        $("#total-available-tweets").text(data.statuses_count);
+      }
+
       user_id = data.id;
       statuses_count = data.statuses_count;
-      
-      if(!data.protected){
+
+      if(!data['protected']){
         // write user to datastore
-        url = twitterAPI + "statuses/user_timeline.json"; 
-        fetchTweetsRecursively(1);        
+        url = twitterAPI + "statuses/user_timeline.json";
+        fetchTweetsRecursively(1);
       } else {
-        // error message if tweets are protected :(        
+        // error message if tweets are protected :(
         $("#tweets").html("<h1>Sorry, but your Tweets are protected and we can't read them :(</h2>");
         $("#status").hide();
       }
     }
+  });
 });
